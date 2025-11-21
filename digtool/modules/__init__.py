c6f53c90f1a7616a024e3b56347d0abc3dec9211
l0uishp <louis.ho-pe@proton.me>
2025-11-21 21:54:02 +0100

    Update __init__.py

--- digtool/modules/__init__.py
+++ digtool/modules/__init__.py
@@ -3,10 +3,12 @@ from digtool.modules.base import BaseModule
 from digtool.modules.gravatar import GravatarModule
 from digtool.modules.site_template import SiteTemplateModule
 from digtool.modules.adobe import AdobeModule
+from digtool.modules.google import GoogleModule
 
 def get_all_modules() -> Dict[str, Type[BaseModule]]:
     return {
         "gravatar": GravatarModule,
         "adobe": AdobeModule,
+        "google": GoogleModule,
         "site_template": SiteTemplateModule
     }
