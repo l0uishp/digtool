7414e1eb5491fd200c57bafeb959fd0bc8788cf7
l0uishp <louis.ho-pe@proton.me>
2025-11-21 23:28:26 +0100

    Update gravatar.py

--- digtool/modules/gravatar.py
+++ digtool/modules/gravatar.py
@@ -16,6 +16,7 @@ class GravatarModule(BaseModule):
             check_url = f"https://www.gravatar.com/{email_hash}.json"
             
             headers = {
+                "Host": "gravatar.com",
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36", 
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
             }
