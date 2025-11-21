4b94151c2a9ef224d9c8368e65c4d81811135e2a
l0uishp <louis.ho-pe@proton.me>
2025-11-21 21:08:59 +0100

    Add AdobeModule import to __init__.py

--- digtool/modules/__init__.py
+++ digtool/modules/__init__.py
@@ -2,6 +2,7 @@ from typing import Dict, Type
 from digtool.modules.base import BaseModule
 from digtool.modules.gravatar import GravatarModule
 from digtool.modules.site_template import SiteTemplateModule
+from digtool.modules.adobe import AdobeModule
 
 def get_all_modules() -> Dict[str, Type[BaseModule]]:
     return {
